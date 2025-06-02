32329d6493d26efe824c319b09cdc027e47e4306
zedlyyy <pedrobarrogalego@hotmail.com>
2025-06-02 17:20:14 +0100

    Fixes

--- Proj2.py
+++ Proj2.py
@@ -16,10 +16,25 @@ def ip_to_location(ip):
     except Exception as e:
         return None
 
+#Exit or not
+def is_possible_exit(relay):
+    exit_policy = relay.get('exit', '').lower()
+    return 'accept' in exit_policy and 'reject *:*' not in exit_policy
+
+#Are two countries in the same alliance
+def is_in_alliance(country_a, country_b, alliances):
+    for group in alliances:
+        if country_a in group and country_b in group:
+            return True
+    return False
+
 with open("Project2ClientInput.json", "r") as f:
     clientData = json.load(f)
 
 with open("tor_consensus.json","r") as f:
     relaysData = json.load(f) 
 
-print(ip_to_location("178.254.37.2"))
+alliances = clientData["Alliances"]
+client_ip = clientData["Client"]
+dest_ip = clientData["Destination"]
+
